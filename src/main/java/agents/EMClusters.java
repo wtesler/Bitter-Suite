caf8e1304dc2b17542382c6c6e657685962b5176
Will Tesler <willtesler@gmail.com>
2015-02-05 23:11:33 -0500

    Created a Normalized EM Clusterer

--- src/main/java/agents/EMClusters.java
+++ src/main/java/agents/EMClusters.java
@@ -7,33 +7,33 @@ import java.util.Random;
 
 public class EMClusters {
 
-    double[][] features;
-    double[][] centroids;
-    double[][] table;
+    private double[][] featuresList;
+    private double[][] centroids;
+    private double[][] table;
 
-    public EMClusters(final double[][] features, final double[] labels, final int k)
-            throws ClusterException {
+    /**
+     * @param featuresList
+     *            A list of <b>normalized</b> feature vectors
+     * @param k
+     *            how many clusters you want.
+     */
+    public EMClusters(final double[][] featuresList, final int k) {
 
-        if (features.length != labels.length) {
-            throw new ClusterException("Features and Labels must be the same length");
-        } else if (features.length < k) {
-            throw new ClusterException("you are requesting more clusters than there are features.");
-        }
+        // All the features.
+        this.featuresList = featuresList;
 
-        this.features = features;
+        // Hold Probability density values for features -> centroids
+        this.table = new double[featuresList.length][k];
 
-        // The probability density value that each features assigns to each
-        // centroid.
-        this.table = new double[features.length][k];
-
-        // A subset of features
-        this.centroids = new double[k][features[0].length];
+        // A subset of features used as centroids. (Wikipedia: Voronoi
+        // Tesselations)
+        this.centroids = new double[k][featuresList[0].length];
 
         initializeCentroids();
 
     }
 
-    public void run() {
+    final public void run() {
         while (true) {
             if (!assign()) {
                 break;
@@ -42,15 +42,21 @@ public class EMClusters {
         }
     }
 
-    private boolean assign() {
+    /**
+     * Assigns probability density values to every features to centroids
+     * pairing.
+     *
+     * @return
+     */
+    final private boolean assign() {
         boolean changed = false;
         // outer-scoped for efficiency but only used in the inner-scope.
         double[] probabilities = new double[centroids.length];
-        for (int i = 0; i < features.length; i++) {
+        for (int i = 0; i < featuresList.length; i++) {
             // Used for normalization
             double sum = 0;
             for (int j = 0; j < centroids.length; j++) {
-                probabilities[j] = probabilityDensity(features[i], centroids[j]);
+                probabilities[j] = probabilityDensity(featuresList[i], centroids[j]);
                 sum += probabilities[j];
             }
             // Normalize Propabilities.
@@ -65,7 +71,11 @@ public class EMClusters {
         return changed;
     }
 
-    private void update() {
+    /**
+     * Updates the positions of the centroids based on the probability density
+     * calculations.
+     */
+    final private void update() {
         // Reset each centroids' values.
         for (int i = 0; i < centroids.length; i++) {
             for (int j = 0; j < centroids[0].length; j++) {
@@ -77,7 +87,7 @@ public class EMClusters {
             // Used for normalization
             double sum = 0;
             // Go through each feature
-            for (int j = 0; j < features.length; j++) {
+            for (int j = 0; j < featuresList.length; j++) {
                 // Add the features probability of being in this centroid to our
                 // normalization sum.
                 sum += table[j][i];
@@ -86,7 +96,7 @@ public class EMClusters {
                     // Each value in the centroid is a summation of each
                     // feature's value multiplied by the probability of seeing
                     // that feature.
-                    centroids[i][k] += features[j][k] * table[j][i];
+                    centroids[i][k] += featuresList[j][k] * table[j][i];
                 }
             }
             // Normalize the centroid's values.
@@ -97,11 +107,11 @@ public class EMClusters {
     }
 
     // Initialize centroids randomly, using Forgy method.
-    private void initializeCentroids() {
+    final private void initializeCentroids() {
 
         // Create our own random list of numbers so that we don't encounter
         // duplicates like we would with Random.nextInt()
-        final int[] random = new int[features.length];
+        final int[] random = new int[featuresList.length];
         // Creates an array like {0,1, 2, .. }
         Arrays.parallelPrefix(random, (left, right) -> {
             return left + 1;
@@ -110,43 +120,30 @@ public class EMClusters {
 
         // Initialize our centroids to randomly selected vectors.
         for (int i = 0; i < centroids.length; i++) {
-            centroids[i] = features[random[i]];
+            centroids[i] = featuresList[random[i]];
         }
     }
 
     // Initialize centroids with a given list of vectors
-    public void setCentroids(final double[][] centroids) {
+    final public void setCentroids(final double[][] centroids) {
         this.centroids = centroids;
     }
 
-    double probabilityDensity(double[] feature, double[] centroid) {
-        double distance = euclidianDistanceSquared(feature, centroid);
+    final double probabilityDensity(double[] feature, double[] centroid) {
+        double similarity = similarity(feature, centroid);
         /*
          * We DO NOT include the Z value 1/sqrt(2pi) in our calculations because
          * this value will normalize anyways.
          */
-        return Math.exp(-0.5 * distance);
+        return Math.exp(-0.5 * similarity);
     }
 
-    private double euclidianDistanceSquared(double[] feature, double[] centroid) {
-        double sum = 0;
-        for (int i = 0; i < feature.length; i++) {
-            sum += Math.pow(feature[i] - centroid[i], 2);
-        }
-
-        /*
-         * We DO NOT Sqrt the sum before returning it since the square wouldn't
-         * change which centroid is nearest.
-         */
-        return sum;
-    }
-
-    public List<double[]> getCentroids() {
+    final public List<double[]> getCentroids() {
         return Collections.unmodifiableList(Arrays.asList(centroids));
     }
 
     // Implementing Fisher–Yates shuffle
-    private static void shuffle(int[] ar) {
+    final private static void shuffle(int[] ar) {
         Random rnd = new Random();
         for (int i = ar.length - 1; i > 0; i--) {
             int index = rnd.nextInt(i + 1);
@@ -157,9 +154,24 @@ public class EMClusters {
         }
     }
 
-    public class ClusterException extends Exception {
-        public ClusterException(String string) {
-            super(string);
+    /**
+     * Euclidian Distance is used to measure similarity between features. Use
+     * subclass NormalizedEMClusters if you want to use Shah and Zhang's fast
+     * similarity algorithm (which requires normalized features).
+     *
+     * @param feature
+     *            vector
+     * @param feature
+     *            vector
+     * @return
+     */
+    public double similarity(double[] feature1, double[] feature2) {
+        int sum = 0;
+        for (int i = 0; i < feature1.length; i++) {
+            sum += Math.pow(feature1[i] - feature2[i], 2);
         }
+        // No need to sqrt.
+        return sum;
     }
+
 }
---
+++ src/main/java/agents/NormalizedEMClusters.java
@@ -0,0 +1,92 @@
+package agents;
+
+import java.util.Arrays;
+
+public class NormalizedEMClusters extends EMClusters {
+
+    public NormalizedEMClusters(double[][] featuresList, int k) {
+        super(featuresList, k);
+        // Check that at least the first features are all normalized.
+        for (int i = 0; i < featuresList[0].length; i++) {
+            if (featuresList[0][i] < 0.0 || featuresList[0][i] > 1.0) {
+                System.err.println("These features don't seem normalized: "
+                        + Arrays.toString(featuresList[0]));
+            }
+        }
+    }
+
+    /**
+     * The equation for <b>variance</b> Since both patterns are assumed to be
+     * normalized, we do not need to include mean in the calculations.
+     *
+     * @param pattern1
+     *            a <b>normalized</b> (mean=0, std=1) vector.
+     * @param pattern2
+     *            a <b>normalized</b> (mean=0, std=1) vector.
+     * @return a similarity rating from <b>-1 to 1.</b>
+     */
+    @Override
+    public double similarity(double[] feature1, double[] feature2) {
+        double sum = 0;
+        for (int i = 0; i < feature1.length; i++) {
+            /*
+             * The calculation here is a simple dot product. Very
+             * computationally easy as long as mean=0 and std=1. That is why the
+             * patterns must be normalized with normalizeVector() before being
+             * passed into this function.
+             */
+            sum += feature1[i] * feature2[i];
+        }
+
+        sum /= feature1.length;
+        // return the variance
+        return sum;
+    }
+
+    /**
+     * Takes any arbitrary pattern and normalizes it in place. Normalize means
+     * the pattern revolves around a mean of 0 and a variance of 1. Note that
+     * this method affects its input directly.
+     *
+     * @param pattern
+     *            a sequence of double representing a time-series window.
+     */
+    public static double[] normalizeVector(double[] features) {
+
+        // Find mean of features.
+        double mean = mean(features);
+
+        // Find variance of features
+        double std = std(features, mean);
+
+        //System.out.format("before; mean: %f variance %f\n", mean, std);
+
+        // Normalize to a mean of 0 and a variance of 1
+        for (int i = 0; i < features.length; i++) {
+            features[i] = (features[i] - mean) / std;
+        }
+        //System.out.format("after; mean: %f variance %f\n", mean(features), std(features, mean(features)));
+        return features;
+    }
+
+    private static double mean(double[] features) {
+        // Find mean of features.
+        double mean = 0;
+        for (int i = 0; i < features.length; i++) {
+            mean += features[i];
+        }
+        mean /= features.length;
+        return mean;
+    }
+
+    private static double std(double[] features, double mean) {
+        // Find variance of features around mean.
+        double variance = 0;
+        for (int i = 0; i < features.length; i++) {
+            variance += Math.pow(features[i] - mean, 2);
+        }
+        variance /= features.length;
+        return Math.sqrt(variance);
+    }
+
+}
